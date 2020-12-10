4b4638f01a4488180642cc17222801cbe1c6cd9d
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-10 13:12:55 -0500

    using bcrypt to compare the unhashed and hashed passwords

--- demo/cd/src/main/java/com/encryption/cd/StarterEncryptionController.java
+++ demo/cd/src/main/java/com/encryption/cd/StarterEncryptionController.java
@@ -34,31 +34,35 @@ public class StarterEncryptionController {
     @GetMapping("/all")
     public Object getAllUsers () {
 
-        return "temp";// return database
+        return database;// return database
     }
     //route for signup test (bcrypt hash)
     @PostMapping("/signup")
-    public String signUp(
+    public User signUp(
             @RequestBody User user
             //grab userdata from body
     ) {
         //hash the password is the first step
+        user.setId();
         String hashedPassword = BCrypt.hashpw(user.password, BCrypt.gensalt());
         user.password = hashedPassword;
         //next step is to then store in the DB
 
         database.put(user.username, user);
-        return "temp";
+        return user;
     }
 
     //route for sign-in test (bcrypt compare + jwt)
     @PutMapping("/signin")
-    public String signIn(
-            //grab userdata from body
-    ) {
-
-
-        return "temp";
+    public String signIn(@RequestBody User user) {
+        try {
+            User loginUser = database.get(user.username);
+            String unhashed = user.password;
+            String hashedPW = loginUser.password;
+            boolean credsMatch = BCrypt.checkpw(unhashed, hashedPW);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
 
     }
 
